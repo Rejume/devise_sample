1a9d99ffc4c1226bc231794fbb59829f9dc36025
rejume <rejumet@gmail.com>
2020-10-14 22:49:11 +0900

    エラーメッセージをコンテンツ毎に表示する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,19 @@ module ApplicationHelper
     end
     html.html_safe
   end
+
+  def full_error_message_on(object, field_name, css_class = 'formError')
+    errors = object.errors[field_name]
+    return "" if errors.blank?
+
+    prepend_text = object.class.human_attribute_name(field_name)
+    if errors.is_a?(Array) && errors.size > 0
+      html = <<-EOF
+        <div class="error_field alert alert-danger" role="alert">
+          <p class="error_msg">#{prepend_text} #{errors.first}</p>
+        </div>
+      EOF
+      html.html_safe
+    end
+  end
 end
